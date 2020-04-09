3a6b28e536ca4a82e0c7b071f017179e389a57ba
Nikolaus Awtrey <nawtrey@asu.edu>
2020-04-09 11:19:02 -0700

    Rearranged code to generate plots and tables from separate files, moved testing code into functions in test_KDA.py, added new panel functionality to plotting.py, changed tables to output as .tex files, tables output probabilities and errors separately, generated all new tables and plots for all models

--- code/plotting.py
+++ code/plotting.py
@@ -1,8 +1,8 @@
 # Nikolaus Awtrey
 # Beckstein Lab
 # Arizona State University
-# 02/14/2020
-# Biochemical Kinetic Diagram Analyzer Plotting
+#
+# Kinetic Diagram Analyzer Plotting
 
 import numpy as np
 import networkx as nx
@@ -10,7 +10,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 
-def plot_input_diagram(G, pos, path=None):
+def plot_input_diagram(G, pos, path=None, label=None):
     """
     Plots the input diagram G.
 
@@ -21,33 +21,27 @@ def plot_input_diagram(G, pos, path=None):
     pos : dict
         Dictionary where keys are the indexed states (0, 1, 2, ..., N) and
         the values are NumPy arrays of x, y coordinates for each node.
+    path : str (optional)
+        String of save path for figure. If path is given figure will be saved
+        at the specified location as 'input_diagram.png'. Default is None.
+    label : str (optional)
+        Figure label, used to create unique figure label if a save path is
+        given. Default is None.
     """
     fig = plt.figure(figsize=(4, 4), tight_layout=True)
     fig.add_subplot(111)
     node_list = [i for i in range(G.number_of_nodes())]
     nx.draw_networkx_nodes(G, pos, node_size=500, nodelist=node_list, node_color='0.8')
-    nx.draw_networkx_edges(G, pos, width=4, arrow_style='->', arrowsize=15)
+    nx.draw_networkx_edges(G, pos, node_size=500, width=4, arrow_style='->', arrowsize=15)
     labels = {}
     for i in range(G.number_of_nodes()):
         labels[i] = r"${}$".format(i+1)
     nx.draw_networkx_labels(G, pos, labels, font_size=16)
     plt.axis('off')
     if not path == None:
-        fig.savefig(path + "/input_diagram.png")
+        fig.savefig(path + "/{}_input_diagram.png".format(label))
 
-def fancy_plot(G, pos, node_sizes):
-    node_list = [i for i in range(G.number_of_nodes())]
-    labels = {}
-    for i in range(G.number_of_nodes()):
-        labels[i] = r"${}$".format(i+1)
-    fig = plt.figure(figsize=(4, 4), tight_layout=True)
-    fig.add_subplot(111)
-    nx.draw_networkx_nodes(G, pos, node_size=node_sizes, nodelist=node_list, node_color='0.8')
-    nx.draw_networkx_edges(G, pos, node_size=node_sizes, arrow_style='->')
-    nx.draw_networkx_labels(G, pos, labels)
-    plt.axis('off')
-
-def plot_partials(partials, pos, panel=False, panel_scale=2, name='partial', path=None):
+def plot_partials(partials, pos, panel=False, panel_scale=1, font_size=12, path=None, label='partial'):
     """
     Plots all partial diagrams.
 
@@ -55,10 +49,30 @@ def plot_partials(partials, pos, panel=False, panel_scale=2, name='partial', pat
     ----------
     partials : list
         List of NetworkX MultiDiGraphs where each graph is a unique partial
-        diagram with no loops.
+        or directional partial diagram with no loops.
     pos : dict
         Dictionary where keys are the indexed states (0, 1, 2, ..., N) and
         the values are NumPy arrays of x, y coordinates for each node.
+    panel : bool (optional)
+        Tells the function to output diagrams as an 'NxM' matrix of subplots,
+        where 'N' and 'M' are determined by the function. True will output panel
+        figure, False will output each figure individually. Default is False.
+    panel_scale : float (optional)
+        Parameter used to scale figure if panel=True. Linearly scales figure
+        height and width. Default is 1.
+    font_size : int (optional)
+        Sets the font size for the figure. Default is 12.
+    path : str (optional)
+        String of save path for figure. If path is given figure will be saved
+        at the specified location. Default is None.
+    label : str (optional)
+        Figure label, used to create unique figure label if a save path is
+        given. Default is 'partial'.
+
+    Notes
+    -----
+    When using panel=True, if number of partials is not a perfect square, extra
+    plots will be generated as empty coordinate axes.
     """
     labels = {}
     for j in range(partials[0].number_of_nodes()):
@@ -70,28 +84,28 @@ def plot_partials(partials, pos, panel=False, panel_scale=2, name='partial', pat
         Ncols = int(np.ceil(N/Nrows))
         fig, ax = plt.subplots(nrows=Nrows, ncols=Ncols, tight_layout=True)
         fig.set_figheight(Nrows*panel_scale)
-        fig.set_figwidth(Ncols*panel_scale)
+        fig.set_figwidth(1.2*Ncols*panel_scale)
         for i, partial in enumerate(partials):
             ix = np.unravel_index(i, ax.shape)
             plt.sca(ax[ix])
-            nx.draw_networkx_nodes(partial, pos, ax=ax[ix], nodelist=node_list, node_color='0.8')
-            nx.draw_networkx_edges(partial, pos, ax=ax[ix], arrow_style='->')
-            nx.draw_networkx_labels(partial, pos, labels, ax=ax[ix])
+            nx.draw_networkx_nodes(partial, pos, ax=ax[ix], node_size=150*panel_scale, nodelist=node_list, node_color='0.8')
+            nx.draw_networkx_edges(partial, pos, ax=ax[ix], node_size=150*panel_scale, arrow_style='->')
+            nx.draw_networkx_labels(partial, pos, labels, font_size=font_size, ax=ax[ix])
             ax[ix].set_axis_off()
         if not path == None:
-            fig.savefig(path + "/{}_diagram_panel.png".format(name))
+            fig.savefig(path + "/{}_diagram_panel.png".format(label))
     else:
         for i, partial in enumerate(partials):
             fig = plt.figure(figsize=(3, 3), tight_layout=True)
             fig.add_subplot(111)
             nx.draw_networkx_nodes(partial, pos, nodelist=node_list, node_color='0.8')
             nx.draw_networkx_edges(partial, pos, arrow_style='->')
-            nx.draw_networkx_labels(partial, pos, labels)
+            nx.draw_networkx_labels(partial, pos, labels, font_size=font_size)
             plt.axis('off')
             if not path == None:
-                fig.savefig(path + "/{}_diagram_{}.png".format(name, i+1))
+                fig.savefig(path + "/{}_diagram_{}.png".format(label, i+1))
 
-def plot_ODE_probs(results, save=None, path=None, ident=None):
+def plot_ODE_probs(results, path=None, label=None):
     """
     Plots probability time series for all states.
 
@@ -100,6 +114,12 @@ def plot_ODE_probs(results, save=None, path=None, ident=None):
     results : bunch object
         Contains time information (results.t) and function information at time
         t (results.y), as well as various other fields.
+    path : str (optional)
+        String of save path for figure. If path is given figure will be saved
+        at the specified location. Default is None.
+    label : str (optional)
+        Figure label, used to create unique figure label if a save path is
+        given. Default is None.
     """
     N = int(len(results.y))
     time = results.t
@@ -114,5 +134,5 @@ def plot_ODE_probs(results, save=None, path=None, ident=None):
     ax.set_ylabel(r"Probability")
     ax.set_xlabel(r"Time (s)")
     ax.legend(loc='best')
-    if save == True:
-        fig.savefig(path + "/ODE_probs_{}.png".format(ident))
+    if not path == None:
+        fig.savefig(path + "/ODE_probs_{}.png".format(label))
