7dc77bc3800aecf97df98ff55bc9858ee1948b8e
unknown <nawtrey@asu.edu>
2020-05-12 12:53:59 -0700

    Added explicit package dependencies

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-"""
-Kinetic Diagram Analysis
-Python package used for the analysis of biochemical kinetic diagrams.
-"""
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+# Kinetic Diagram Analysis
+#
+# Released under the GNU Public Licence, v3 or any higher version
+
 import sys
 from setuptools import setup, find_packages
 import versioneer
 
 short_description = __doc__.split("\n")
-
-# from https://github.com/pytest-dev/pytest-runner#conditional-requirement
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
 pytest_runner = ['pytest-runner'] if needs_pytest else []
 
@@ -24,36 +25,31 @@ setup(
     name='kda',
     author='Nikolaus Awtrey',
     author_email='nawtrey@asu.edu',
+    url='https://github.com/Becksteinlab/kda/',
     description=short_description[0],
     long_description=long_description,
     long_description_content_type="text/markdown",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     license='LGPLv3',
-
-    # Which Python importable modules should be included when your package is installed
-    # Handled automatically by setuptools. Use 'exclude' to prevent some specific
-    # subpackage(s) from being added, if needed
+    keywords="science kinetic",
+    project_urls={
+        'Source': 'https://github.com/Becksteinlab/kda/',
+        'Issue Tracker': 'https://github.com/Becksteinlab/kda/issues',
+    },
     packages=find_packages(),
-
-    # Optional include package data to ship with your package
-    # Customize MANIFEST.in if the general case does not suit your needs
-    # Comment out this line to prevent the files from being packaged with your software
+    install_requires=[
+        'numpy',
+        'networkx',
+        'scipy',
+        'sympy',
+        'functools',
+        'itertools',
+        'matplotlib',
+    ],
+    tests_require=[
+        'pytest',
+    ],
     include_package_data=True,
-
-    # Allows `setup.py test` to work correctly with pytest
     setup_requires=[] + pytest_runner,
-
-    # Additional entries you may want simply uncomment the lines you want and fill in the data
-    # url='http://www.my_package.com',  # Website
-    # install_requires=[],              # Required packages, pulls from pip if needed; do not use for Conda deployment
-    # platforms=['Linux',
-    #            'Mac OS-X',
-    #            'Unix',
-    #            'Windows'],            # Valid platforms your code works on, adjust to your flavor
-    # python_requires=">=3.5",          # Python version restrictions
-
-    # Manual control if final package is compressible or not, set False to prevent the .egg from being made
-    # zip_safe=False,
-
-)
+    )
