254155f891afc8f7a4a99b34bda1b639f63ab691
Nikolaus C. Awtrey <nawtrey@asu.edu>
2024-08-13 21:23:19 -0700

    Add airspeed velocity benchmarks
    * Add `asv` configuration file
    
    * Add two benchmark files: `bench_diagrams.py`
    for benchmarking functions in `diagrams.py` and
    `bench_calculations.py` for benchmarking
    functions in `calculations.py`
    
    * Add `requirements_dev.txt` for easy
    installation of required packages for
    `asv` benchmarking and testing with
    `pytest`. Will eventually be used in CI.

---
+++ benchmarks/benchmarks/bench_calculations.py
@@ -0,0 +1,152 @@
+# Write the benchmarking functions here.
+# See "Writing benchmarks" in the asv docs for more information.
+
+import numpy as np
+import networkx as nx
+
+from kda import graph_utils, calculations, diagrams
+
+
+def build_graph(graph):
+    if graph == "3-state":
+        # 3-state model, simplest test model
+        K = np.array([
+            [0, 1, 1],
+            [1, 0, 1],
+            [1, 1, 0],
+        ])
+    elif graph == "Hill-5-state":
+        # 5-state model with leakage, used extensively in T.L. Hill's
+        # "Free Energy Transduction and Biochemical Cycle Kinetics"
+        K = np.array([
+            [0, 1, 1, 0, 0],
+            [1, 0, 1, 1, 0],
+            [1, 1, 0, 0, 1],
+            [0, 1, 0, 0, 1],
+            [0, 0, 1, 1, 0],
+        ])
+
+    elif graph == "Hill-8-state":
+        # 8-state model with leakage used for flux diagram example in
+        # T.L. Hill's "Free Energy Transduction and Biochemical Cycle Kinetics"
+        K = np.array([
+            [0, 1, 1, 0, 0, 0, 0, 0],
+            [1, 0, 0, 1, 0, 0, 0, 0],
+            [1, 0, 0, 1, 1, 0, 0, 0],
+            [0, 1, 1, 0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0, 1, 1, 0],
+            [0, 0, 0, 1, 1, 0, 0, 1],
+            [0, 0, 0, 0, 1, 0, 0, 1],
+            [0, 0, 0, 0, 0, 1, 1, 0],
+        ])
+    elif graph == "EmrE-8-state":
+        # EmrE 8-state model
+        K = np.array([
+            [0, 1, 1, 0, 0, 0, 1, 0],
+            [1, 0, 0, 1, 0, 0, 0, 1],
+            [1, 0, 0, 1, 1, 0, 0, 0],
+            [0, 1, 1, 0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0, 1, 1, 0],
+            [0, 0, 0, 1, 1, 0, 0, 1],
+            [1, 0, 0, 0, 1, 0, 0, 1],
+            [0, 1, 0, 0, 0, 1, 1, 0],
+        ])
+    G = nx.MultiDiGraph()
+    graph_utils.generate_edges(G=G, vals=K)
+    return G
+
+
+class StateProbs:
+    """
+    A benchmark to test the time and space complexity of the
+    `calculations.calc_state_probs()` function.
+    """
+    param_names = ["graph", "output_strings"]
+    params = [
+        [
+            "3-state",
+            "Hill-5-state",
+            "Hill-8-state",
+            "EmrE-8-state",
+        ],
+        [
+            True,
+            False,
+        ]
+    ]
+
+    def setup(self, graph, output_strings):
+        # build the kinetic diagram and store it for use
+        # in the time and memory tests
+        if output_strings:
+            key = "name"
+        else:
+            key = "val"
+        self.key = key
+        self.G = build_graph(graph=graph)
+
+    def time_calc_state_probs(self, graph, output_strings):
+        # benchmark state probability calculation algorithm
+        # for various models we commonly use for testing
+        calculations.calc_state_probs(
+            G=self.G,
+            key=self.key,
+            output_strings=output_strings,
+        )
+
+    def peakmem_calc_state_probs(self, graph, output_strings):
+        calculations.calc_state_probs(
+            G=self.G,
+            key=self.key,
+            output_strings=output_strings,
+        )
+
+
+class Sigma:
+    """
+    A benchmark to test the time and space complexity of the
+    `calculations.calc_sigma()` function.
+    """
+    param_names = ["graph", "output_strings"]
+    params = [
+        [
+            "3-state",
+            "Hill-5-state",
+            "Hill-8-state",
+            "EmrE-8-state",
+        ],
+        [
+            True,
+            False,
+        ]
+    ]
+
+    def setup(self, graph, output_strings):
+        # build the kinetic diagram and directional diagram edges
+        # and store them for use in the time and memory benchmarks
+        if output_strings:
+            key = "name"
+        else:
+            key = "val"
+        self.key = key
+        self.G = build_graph(graph=graph)
+        self.dir_edges = diagrams.generate_directional_diagrams(
+            G=self.G, return_edges=True)
+
+    def time_calc_sigma(self, graph, output_strings):
+        # benchmark sigma calculation algorithm
+        # for various models we commonly use for testing
+        calculations.calc_sigma(
+            G=self.G,
+            dirpar_edges=self.dir_edges,
+            key=self.key,
+            output_strings=output_strings,
+        )
+
+    def peakmem_calc_sigma(self, graph, output_strings):
+        calculations.calc_sigma(
+            G=self.G,
+            dirpar_edges=self.dir_edges,
+            key=self.key,
+            output_strings=output_strings,
+        )
